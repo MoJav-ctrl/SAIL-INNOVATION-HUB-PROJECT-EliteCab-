3a854cb7e39aff4c03bdde34a8e585cf0368a6c7
MoJav-ctrl <mojavi22@gmail.com>
2025-04-07 09:51:26 +0100

    updated codebase

--- models/rideModel.js
+++ models/rideModel.js
@@ -23,19 +23,40 @@ const RideSchema = new mongoose.Schema(
     availableSeats: { 
       type: Number, 
       required: true, 
-      min: 1 
+      min: 0
     },
 
-    passengers: [{ 
-      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-      status: { type: String, enum: ['pending', 'approved'], default: 'pending' }
+    // passengers: [{ 
+    //   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    //   status: { type: String, enum: ['pending', 'approved'], default: 'pending' }
+    // }],
+
+    passengers: [{
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        status: { 
+            type: String, 
+            enum: ['pending', 'accepted', 'rejected'],
+            default: 'pending'
+        },
+        requestedAt: { type: Date, default: Date.now },
+        approvedAt: Date, // Track when request was approved
+        pickupLocation: {type: String, trim: true},
+        dropoffLocation: {type: String, trim: true}
     }],
 
     status: { 
       type: String, 
       enum: ["open", "full", "completed"], 
       default: "open" 
-    }
+    },
+    distance: {
+      type: Number,
+      required: true
+  },
+    duration: {
+      type: Number,
+      required: true
+  }, 
   }, 
   { timestamps: true }
 );
